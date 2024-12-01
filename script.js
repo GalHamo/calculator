91d10ba01eb856e2d1a573f71cfd793de17a107a
Gal <galhamo114@gmail.com>
2024-12-01 14:06:37 +0200

    did some changes on logic of the button to make sure every button displays itself to the screen correctly
    also did a little border radius to the viewport

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const buttContainer = document.querySelector('.number-buttons')
 const resultScreen = document.querySelector('.show-result')
+let currInput = ''
+let currExpress = ''
 
 const numButtGen = () => {
     let numButtArr = []
@@ -15,24 +17,51 @@ const numButtGen = () => {
         numButt.innerHTML = `${btn.id}`
         numButt.addEventListener('click', () => {
             console.log(btn.id)
-            resultScreen.innerHTML += `${btn.id}`
+            currInput += `${btn.id}`
+            resultScreen.innerHTML = currInput
         })
         buttContainer.appendChild(numButt)
 })
 }
 
 const expressGen = () => {
+    let expressButtArr = []
     const expressions = ['+', '-', '*', '/', 'C', '=']
     for (let i = 0; i < expressions.length; i++){
+
+        let expressObj = {
+            express : expressions[i]
+        }
+        expressButtArr.push(expressObj)
+    }
+    
+    expressButtArr.forEach((btn) => {
         let expressButt = document.createElement('button')
         expressButt.classList.add('express-butt')
-        expressButt.innerHTML = `${expressions[i]}`
+        expressButt.innerHTML = `${btn.express}`
         if (expressButt.innerHTML === 'C'){
             expressButt.classList.add('delete-butt')
         }
-        
+        expressButt.addEventListener('click', (e) => {
+            currExpress = `${e.target.innerHTML}`
+            console.log(currExpress)
+            if (currExpress === '='){
+                currExpress = ''
+                currInput = ''
+                resultScreen.innerHTML = ''
+            }
+            if (currExpress === 'C'){
+                console.log('y')
+                console.log(currInput)
+                currInput = currInput.slice(0, currInput.length - 1)
+                resultScreen.innerHTML = currInput
+            } else {
+                currInput += currExpress
+                resultScreen.innerHTML += `${currExpress}`}
+                currExpress = ''
+        })
         buttContainer.appendChild(expressButt)
-    }
+    })
 }
 numButtGen()
 expressGen()
